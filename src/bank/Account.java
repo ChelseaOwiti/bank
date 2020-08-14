6f04df6d7158cd1e111dc4406c7d6e702399f0c2
Chelsea Owiti <chelseaowiti@gmail.com>
2020-08-14 12:10:50 +0300

    Repo amade

---
+++ src/bank/Account.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ *
+ * @author chelsea
+ */
+public class Account {
+   private int id= 0;
+   private double balance = 0;
+   private double input = 0;
+   private double withdraw = balance - input;
+   private double deposit = balance + input;
+    private String JOptionPane;
+   
+   
+//    
+    public double getBalance(){
+    
+    return this.balance;
+    }
+    public double withdraw(double withdraw){
+   //tring input = JOptionPane.showInputDialog(null,"Enter amount to withdraw","Withdrawal",JOptionPane.;
+    double withAm = 0;
+       return 0;
+    
+    }
+//   
+    
+   
+   
+   
+   
+   
+   
+   
+   
+}
---
+++ src/bank/tempCodeRunnerFile.java
@@ -0,0 +1,32 @@
+ // private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    //     if(evt.getSource()== jButton1){
+				
+                
+    //           try{
+    //               Class.forName("com.mysql.jdbc.Driver");
+    //               Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/bank", "root", "");
+    //               String sql = "Select * from Persons WHERE Accountno=? and Password=?";
+    //               PreparedStatement pst = con.prepareStatement(sql);
+    //               pst.setString(1, username.getText());
+    //               pst.setString(2, password.getText());
+    //               ResultSet rst = pst.executeQuery();
+    //               if(rst.next()){
+    //                 //   Loginbox a = new Loginbox();
+    //                 //   a.setVisible(true);
+    //                 //   this.setVisible(false);
+    
+    //               }
+    //               else {
+    //                   JOptionPane.showMessageDialog(null, "Invlid Credentials");
+    //               }
+    //               dispose();
+                
+    //           } catch(SQLException ex){
+    //               Logger.getLogger(Bankika.class.getName()).log(Level.SEVERE, null, ex);
+             
+    //           } catch (ClassNotFoundException ex) {
+    //             Logger.getLogger(Bankika.class.getName()).log(Level.SEVERE, null, ex);
+    //         }
+
+    //     }
+    // }
